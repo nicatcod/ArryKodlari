ec64cfdb777595c46626110d94e8d1ebeb1381b0
nicatcod <qaqa0qaqa002@gmail.com>
2024-07-01 16:16:38 +0400

    ArreyYeni

--- src/Eolymp1.java
+++ src/Eolymp1.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Eolymp1 {
+import java.util.Scanner;
+
+public class Eolymp1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[][] matrix = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = scanner.nextInt();
+
+            }
+        }
+        int r = scanner.nextInt();
+        int c = scanner.nextInt();
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,40 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+//n tam ədəddən ibarət massiv verilir.
+// Onun bütün ən böyük elementlərinin qiymətini ən kiçiklə, ən kiçiklərini isə ən böyüklə əvəzləyin.
+        Scanner scanner = new Scanner(System.in);{
+            System.out.print("Daxil etdiyin ededin sayi:");
+        }
+        int n = scanner.nextInt();{
+            System.out.println("Ededleri daxil et:");
+        }
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        int max = arr[0];
+        int min = arr[0];
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+            if (arr[i] < min) {
+                min = arr[i];
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            }
+
+        }
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == max) {
+                arr[i] = min;
+            } else if (arr[i] == min) {
+                arr[i] = max;
+            }
+
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
         }
     }
 }
